d114b5c99a2fd92ab5ae571daec7196dc851f26d
Brian Kim <brk99@pitt.edu>
2026-06-25 23:01:55 +0900

    feat: initialize project structure with main app and content view

---
+++ Shopping/Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Brian Kim on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
